2305ec8f4baf5bfc5209e82d980a2050a401a34d
Amanda <ajnikrant@gmail.com>
2021-03-18 19:31:34 -0400

    commit tree a74705426a5549b4323e1745a0e4b0d612b267a8

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artist1= Artist.create(name:"Beyonce", bio: "shes the queen")
+artist2= Artist.create(name:"freddy mercury", bio: "hes also the queen")
+artist3= Artist.create(name:"jay z", bio: "married to the queen")
+
+genre1= Genre.create(name: "jazz")
+genre2= Genre.create(name: "rock")
+genre3= Genre.create(name: "rnb")
+genre4= Genre.create(name: "classical")
+
+Song.create(name: "halo", artist_id: artist1.id, genre_id: genre3.id)
+Song.create(name: "someone to love", artist_id: artist2.id, genre_id: genre2.id)
+Song.create(name: "some song", artist_id: artist3.id, genre_id: genre4.id)
+Song.create(name: "hello", artist_id: artist1.id, genre_id: genre3.id)
+
+puts "seeded"
